fcde83bc4c2ad792a5c6c8433a9a091d3639b2a1
ltourne <layneetourne@yahoo.com>
2022-10-19 18:45:17 -0500

    Removed excess VCs, add tab view controller, design updates

--- CS329E_mainProject/CS329E_mainProject/CookbookViewController.swift
+++ CS329E_mainProject/CS329E_mainProject/CookbookViewController.swift
@@ -7,35 +7,12 @@
 
 import UIKit
 
-let cookbookMenu = ["Favorites", "Your Categories", "Recipes by Ingredient", "Randomize"]
-let textCellIdentifier = "TextCell"
-
-class CookbookViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class CookbookViewController: UIViewController {
     
-    @IBOutlet weak var cookbookTV: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        cookbookTV.delegate = self
-        cookbookTV.dataSource = self
 
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cookbookMenu.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let row = indexPath.row
-        let cell = cookbookTV.dequeueReusableCell(withIdentifier: textCellIdentifier, for: indexPath)
-        cell.textLabel?.text = cookbookMenu[row]
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let row = indexPath.row
-        cookbookTV.deselectRow(at: indexPath, animated: true)
     }
 
 }
