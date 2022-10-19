528f813982a3408958d5e4f9d7f98b95064ff63f
Krystal <eddithfigkyto@gmail.com>
2022-10-19 10:09:12 -0500

    Comments to UserSettings VC

--- CS329E_mainProject/CS329E_mainProject/UserSettings/UserSettingsViewController.swift
+++ CS329E_mainProject/CS329E_mainProject/UserSettings/UserSettingsViewController.swift
@@ -7,17 +7,21 @@
 
 import UIKit
 
+//segue identifiers for VCs associated with each button on menu
 let personalInformationSegueIdentifier:String = "personalInformationSegue"
 let dietSegueIdentifier:String = "dietSegue"
 
+//Protocol definition that allows the name of the user to be updated
 protocol changeNameProtocol{
     func changeName(newName:String)
 }
 
 class UserSettingsViewController: UIViewController, changeNameProtocol{
     
+    //variable to store the user's name
     public var name = "Kyto"
     
+    //Outlets for labels and buttons on the VC
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var personalInformationLabel: UIButton!
     @IBOutlet weak var dietFiltersLabel: UIButton!
@@ -25,12 +29,15 @@ class UserSettingsViewController: UIViewController, changeNameProtocol{
     @IBOutlet weak var manageFriendsLabel: UIButton!
     @IBOutlet weak var privacyPolicyButton: UIButton!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //Set greeting to user's name and change font to Avenir Next, size 22
         nameLabel.text = "Hello \(name)!"
         nameLabel.font = UIFont(name:"Avenir Next", size:22)
         
+        //Change font of all buttons to Avenir Next, size 18
         personalInformationLabel.titleLabel?.font = UIFont(name:"Avenir Next", size:18)
         dietFiltersLabel.titleLabel?.font = UIFont(name:"Avenir Next", size:18)
         notificationsLabel.titleLabel?.font = UIFont(name:"Avenir Next", size:18)
@@ -39,6 +46,7 @@ class UserSettingsViewController: UIViewController, changeNameProtocol{
         
     }
     
+    //when segue to personal information VC is triggered, send over self as delegate and name as currentName
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == personalInformationSegueIdentifier,
            let nextVC = segue.destination as? PersonalInformationViewController {
@@ -47,6 +55,7 @@ class UserSettingsViewController: UIViewController, changeNameProtocol{
         }
     }
     
+    //function defined in protocol that updates the user's name if the user changes it. 
     func changeName(newName: String) {
         name = newName
         self.viewDidLoad()
