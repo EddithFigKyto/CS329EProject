5820ab7ce79a87a71f6dc4c0d797bb7529b94a0f
Krystal <eddithfigkyto@gmail.com>
2022-11-01 19:40:59 -0500

    11/01 Font Progress

--- CS329E_mainProject/CS329E_mainProject/Cookbook/UserSettings/FontPickerViewController.swift
+++ CS329E_mainProject/CS329E_mainProject/Cookbook/UserSettings/FontPickerViewController.swift
@@ -7,23 +7,48 @@
 
 import UIKit
 
-class FontPickerViewController: UIViewController {
+let font = ["Avenir Next", "American Typewriter", "Menlo-Regular", "Papyrus"]
 
+public var fontSet = font[0]
+
+class FontPickerViewController: UIViewController {
+    
+    @IBOutlet weak var fontDisplay: UILabel!
+    
+    
+    @IBOutlet weak var AvenirNextLabel: UILabel!
+    @IBOutlet weak var AvenirNextSwitch: UISwitch!
+    
+    
+    @IBOutlet weak var AmericanTypewriterLabel: UILabel!
+    @IBOutlet weak var AmericanTypewriterSwitch: UISwitch!
+    
+    
+    @IBOutlet weak var MenloRegularLabel: UILabel!
+    @IBOutlet weak var MenloRegularSwitch: UISwitch!
+    
+    
+    @IBOutlet weak var PapyrusLabel: UILabel!
+    @IBOutlet weak var PapyrusSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //personalInformationLabel.configuration?.attributedTitle?.font = UIFont(name:"Avenir Next", size:18)
+        
+        fontDisplay.font = UIFont(name: fontSet, size: 18)
+        AvenirNextLabel.font = UIFont(name: "Avenir Next", size: 18)
+        AmericanTypewriterLabel.font = UIFont(name: "American Typewriter", size: 18)
+        MenloRegularLabel.font = UIFont(name: "Menlo-Regular", size: 18)
+        PapyrusLabel.font = UIFont(name: "Papyrus", size: 18)
+        
+        
+        
 
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
