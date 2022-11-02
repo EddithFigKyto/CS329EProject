0547dd7350e36d3cc9b69b960ded89e2091011f5
Krystal <eddithfigkyto@gmail.com>
2022-11-02 17:15:14 -0500

    11/02 pt 2

--- CS329E_mainProject/CS329E_mainProject/Cookbook/UserSettings/CustomizationViewController.swift
+++ CS329E_mainProject/CS329E_mainProject/Cookbook/UserSettings/CustomizationViewController.swift
@@ -10,17 +10,23 @@ import UIKit
 class CustomizationViewController: UIViewController {
     
     
+    @IBOutlet weak var ColorSchemeLabel: UIButton!
+    
+    @IBOutlet weak var FontButtonLabel: UIButton!
     
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func fontPickerButtonPressed(_ sender: Any) {
+        
+        FontButtonLabel.configuration?.attributedTitle?.font = UIFont(name: fontSet, size: 18)
+        ColorSchemeLabel.configuration?.attributedTitle?.font = UIFont(name: fontSet, size: 18)
+        
     }
     
-
+    override func viewWillAppear(_ animated: Bool) {
+        FontButtonLabel.configuration?.attributedTitle?.font = UIFont(name: fontSet, size: 18)
+        ColorSchemeLabel.configuration?.attributedTitle?.font = UIFont(name: fontSet, size: 18)
+        
+    }
+    
 }
