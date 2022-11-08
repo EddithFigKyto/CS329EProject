356b5df062a547190b06f0e5bab9b0465019d32b
Krystal <eddithfigkyto@gmail.com>
2022-11-08 12:51:01 -0600

    pt3

--- CS329E_mainProject/CS329E_mainProject/Cookbook/UserSettings/CustomDefaults.swift
+++ CS329E_mainProject/CS329E_mainProject/Cookbook/UserSettings/CustomDefaults.swift
@@ -11,6 +11,22 @@ class CustomDefaults{
     
     let userCustomizations = UserDefaults.standard
     
+    func setUp(){
+        
+        //keys for defaults
+        let uNameKey = "user name"
+        let uBirthdayKey = "birthday"
+        let uFontKey = "font"
+        
+        //values for associated keys
+        let name = "Jane Doe"
+        let birthday = "07/17/1999"
+        let font = "Avenir Next"
+        
+    }
+    
+    
+    
     
     
 }
