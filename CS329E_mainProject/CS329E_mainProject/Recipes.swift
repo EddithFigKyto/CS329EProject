99df8495bc5f157894d1915eed5f17f7d3bdcbf0
Krystal <eddithfigkyto@gmail.com>
2022-11-10 15:12:05 -0600

    Delete random file and comment vars in Recipes class 11/10

--- CS329E_mainProject/CS329E_mainProject/Recipes.swift
+++ CS329E_mainProject/CS329E_mainProject/Recipes.swift
@@ -8,7 +8,8 @@
 import Foundation
 import UIKit
 
-
+/*
 var recipe1 = Recipe(creator: "Liquor.com", title: "Tequila Sunrise", ingredients: ["2 ounces blanco tequila", "4 ounces orange juice, freshly squeezed", "1/4 ounce grenadine", "Garnish: orange slice", "Garnish: cherry"], servingSize: 1, cuisine: "Drinks", description: ["The Tequila Sunrise cocktail, with its bright striations of color, evokes a summer sunrise. This classic drink has only three ingredients—tequila, grenadine and orange juice—and is served unmixed to preserve the color of each layer."], dietaryRestriction: Recipe.diet.vegan, dish: Recipe.dishType.drinks, time: 5, image: UIImage(named: "tequila_sunrise")!)
 
 var recipe2 = Recipe(creator: "Love and Lemons", title: "Tequila Sunrise", ingredients: ["¼ cup extra-virgin olive oil", "3 tablespoons red wine vinegar", "1 garlic clove, minced", "½ teaspoon dried oregano, more for sprinkling", "¼ teaspoon Dijon mustard", "¼ teaspoon sea salt", "Freshly ground black pepper", "1 English cucumber, cut lengthwise, seeded, and sliced ¼-inch thick", "1 green bell pepper, chopped into 1-inch pieces", "2 cups halved cherry tomatoes", "5 ounces feta cheese, cut into ½ inch cubes*", "⅓ cup thinly sliced red onion", "⅓ cup pitted Kalamata olives", "⅓ cup fresh mint leaves"], servingSize: 4, cuisine: "Greek", description: ["This easy Greek salad recipe is a flavorful, refreshing summer side dish! If you make it ahead for a gathering, save a few mint leaves to add right before serving."], dietaryRestriction: Recipe.diet.vegan, dish: Recipe.dishType.salad, time: 15, image: UIImage(named: "greek_salad")!)
+*/
