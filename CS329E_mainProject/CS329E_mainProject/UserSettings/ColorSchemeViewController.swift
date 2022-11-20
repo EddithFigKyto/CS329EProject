d173bd946836155b75caeb231f5ae6bfd82ae143
Krystal <eddithfigkyto@gmail.com>
2022-11-20 13:31:39 -0600

    11/20 colorScheme progress

--- CS329E_mainProject/CS329E_mainProject/UserSettings/ColorSchemeViewController.swift
+++ CS329E_mainProject/CS329E_mainProject/UserSettings/ColorSchemeViewController.swift
@@ -8,10 +8,53 @@
 import UIKit
 
 class ColorSchemeViewController: UIViewController {
+    
+    @IBOutlet weak var colorSchemeTextDisplay: UILabel!
+    @IBOutlet weak var instructionLabel: UILabel!
+    
+    
+    @IBOutlet weak var pinkSwitch: UISwitch!
+    @IBOutlet weak var pinkLabel: UILabel!
+    
+    @IBOutlet weak var cyanSwitch: UISwitch!
+    @IBOutlet weak var cyanLabel: UILabel!
+    
+    @IBOutlet weak var blackSwitch: UISwitch!
+    @IBOutlet weak var blackLabel: UILabel!
+    
+    @IBOutlet weak var purpleSwitch: UISwitch!
+    @IBOutlet weak var purpleLabel: UILabel!
+    
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var greenLabel: UILabel!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //
+    }
+    
+    @IBAction func pinkPressed(_ sender: UISwitch) {
+    }
+    
+    @IBAction func cyanPressed(_ sender: UISwitch) {
+    }
+    
+    @IBAction func blackPressed(_ sender: UISwitch) {
+    }
+    
+    @IBAction func purplePressed(_ sender: UISwitch) {
+    }
+    
+    @IBAction func greenPressed(_ sender: UISwitch) {
+    }
+    
     
 
 }
