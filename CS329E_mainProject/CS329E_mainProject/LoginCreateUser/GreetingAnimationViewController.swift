4a0f894694506e9412be9df1e702bdeaa77953cf
Krystal <eddithfigkyto@gmail.com>
2022-11-25 06:15:15 -0600

    Created files for Create User Path Vis

---
+++ CS329E_mainProject/CS329E_mainProject/LoginCreateUser/GreetingAnimationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GreetingAnimationViewController.swift
+//  CS329E_mainProject
+//
+//  Created by Eddith Figueroa on 11/25/22.
+//
+
+import UIKit
+
+class GreetingAnimationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
