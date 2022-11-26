524b2069561ae2e277018a925b808a7cb1e36622
Najia Sherwani <najiasherwani@utexas.edu>
2022-11-26 05:39:23 -0600

    work

---
+++ CS329E_mainProject/CS329E_mainProject/HomeTableViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  HomeTableViewCell.swift
+//  CS329E_mainProject
+//
+//  Created by Najia Khan Sherwani on 2022-11-26.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+    //    static let identifier = "homeCell"
+    //
+    //    static func nib() -> UINib {
+    //        return UINib(nibName: "homeCell", bundle: nil)
+    //    }
+    //    public func configure(with title: String, description: String, imageName: String) {
+    //        image.image = UIImage(systemName: imageName)
+    //        descriptionLabel.text = description
+    //        titleLabel.text = title
+    //    }
+  
+  
+    @IBOutlet weak var picture: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- CS329E_mainProject/CS329E_mainProject/HomeVC.swift
+++ CS329E_mainProject/CS329E_mainProject/HomeVC.swift
@@ -7,23 +7,77 @@
 
 import UIKit
 
-class HomeVC: UIViewController {
 
+
+class HomeVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //        tableView.register(homeCell.nib(), forCellReuseIdentifier: "homeCell")
+        addNavBarImage()
+        tableView.dataSource = self
+        tableView.delegate = self
+   
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+        
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipes.count
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = indexPath.row
+        let cell = tableView.dequeueReusableCell(withIdentifier: "homeCell", for: indexPath) as! HomeTableViewCell
+        
+        cell.titleLabel.text = recipes[row].title
+        cell.descriptionLabel.text = recipes[row].description[0]
+        cell.picture.image = UIImage(named: "greek_salad")
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        let imageURL = URL(string: recipes[row].recipeImage)!
+        
+        let session = URLSession(configuration: .default)
+        
+        // Create a task for accessing the image
+        let task = session.dataTask(with: imageURL) {
+            (data, response, error) in
+            
+            guard error == nil else { return }
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                
+                // ensure that we got a response code of 200 (which means "success")
+                guard httpResponse.statusCode == 200 else { return }
+                
+                if let receivedData = data {
+                    DispatchQueue.main.async {
+//                        cell.recipeImage.image = UIImage(data: receivedData)
+                    }
+                }
+            }
+        }
+        task.resume()
+        
+        return cell
     }
-    */
+    
+    func addNavBarImage() {
+        
+        var titleView = UIView(frame: CGRectMake(0, 0, 130, 40))
+        var titleImageView = UIImageView(image: UIImage(named: "banner1"))
+        titleImageView.frame = CGRectMake(0, 0, titleView.frame.width, titleView.frame.height)
+        titleView.addSubview(titleImageView)
+        navigationItem.titleView = titleView
+    }
+
+
+  
 
 }
