4955233c5881a5d37f0be55c250d0b4bd57855a2
ltourne <layneetourne@yahoo.com>
2022-11-27 19:30:42 -0600

    MapKit basics added

--- CS329E_mainProject/CS329E_mainProject/Cookbook/restaurantViewController.swift
+++ CS329E_mainProject/CS329E_mainProject/Cookbook/restaurantViewController.swift
@@ -13,32 +13,33 @@ class restaurantViewController: UIViewController, MKMapViewDelegate, CLLocationM
     
     @IBOutlet weak var mapView: MKMapView!
     
-    var locationManager = CLLocationManager()
+    var manager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.requestLocation()
-        
-        locationManager.delegate = self
-        
-        mapView.delegate = self
-        mapView.showsUserLocation = true
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
     }
-    */
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            manager.stopUpdatingLocation()
+            
+            render(location)
+        }
+    }
+    
+    func render(_ location: CLLocation){
+        let coordinate = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+    }
 
 }
