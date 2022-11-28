d5bb01b1f4a0321fae50d24c140fda8751a270e9
Carla Gonzalez <carlagonzalez@utexas.edu>
2022-11-28 00:34:43 -0600

    .

--- CS329E_mainProject/CS329E_mainProject/groceryListViewController.swift
+++ CS329E_mainProject/CS329E_mainProject/groceryListViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class groceryListViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    var items = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,6 +18,31 @@ class groceryListViewController: UIViewController {
     }
     
     @IBAction func addItem(_ sender: Any) {
+        let controller = UIAlertController(
+            title: "Grocery list",
+            message: "Add item",
+            preferredStyle: .alert)
+        controller.addAction(UIAlertAction(
+            title: "Cancel",
+            style: .cancel))
+        controller.addTextField(configurationHandler: {
+            (textField:UITextField!) in
+            textField.placeholder = "Enter something"
+        })
+        controller.addAction(UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: {
+                (paramAction:UIAlertAction!) in
+                if let textFieldArray = controller.textFields {
+                    let textFields = textFieldArray as [UITextField]
+                    let enteredText = textFields[0].text
+                    print(enteredText!)
+                }
+            }))
+        present(controller, animated: true)
+        
+        
     }
     
     
