9aa7652e3a2f17ce44963f3a16336fd56199101e
ltourne <layneetourne@yahoo.com>
2022-11-27 20:46:48 -0600

    scroll views added

--- CS329E_mainProject/CS329E_mainProject/Cookbook/randomizeViewController.swift
+++ CS329E_mainProject/CS329E_mainProject/Cookbook/randomizeViewController.swift
@@ -27,7 +27,44 @@ class randomizeViewController: UIViewController {
     
     @IBAction func randomizeButtonPress(_ sender: Any) {
         var recipeNum = randomize()
-        titleLabel.text = recipes[recipeNum].title
+        let selectedRecipe = recipes[recipeNum]
+       // otherVC.sendAllInfo(someRecipe: selectedRecipe)
+
+//        send data to next VC
+        let vc = self.storyboard!.instantiateViewController(withIdentifier: "ScrollRecipeViewController") as! ScrollRecipeViewController
+        
+        vc.title1 = selectedRecipe.title
+        vc.description1 = selectedRecipe.description[0]
+        vc.ingredients1 = selectedRecipe.printIngredients
+        vc.stepList1 = selectedRecipe.stepList
+        vc.timersList1 = selectedRecipe.timersList
+        vc.tags1 = selectedRecipe.tags.joined(separator: ", ")
+        
+        let imageURL = URL(string: recipes[recipeNum].recipeImage)!
+        
+        let session = URLSession(configuration: .default)
+        
+        let task = session.dataTask(with: imageURL) {
+            (data, response, error) in
+            
+            guard error == nil else { return }
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                
+                // ensure that we got a response code of 200 (which means "success")
+                guard httpResponse.statusCode == 200 else { return }
+                
+                if let receivedData = data {
+                    DispatchQueue.main.async {
+                        vc.imageView.image = UIImage(data: receivedData)
+                    }
+                }
+            }
+        }
+        task.resume()
+        
+        // the following segues to the next screen while pushing the appropriate cell data
+        self.navigationController?.pushViewController(vc, animated: true)
         
     }
     
