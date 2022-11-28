bde0e4c3ad56c581775728d8023affe577b8a083
Carla Gonzalez <carlagonzalez@utexas.edu>
2022-11-27 22:58:39 -0600

    .

--- CS329E_mainProject/CS329E_mainProject/LoginCreateUser/loginViewController.swift
+++ CS329E_mainProject/CS329E_mainProject/LoginCreateUser/loginViewController.swift
@@ -12,12 +12,18 @@ class loginViewController: UIViewController {
 
     @IBOutlet weak var userTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var segControl: UISegmentedControl!
     @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var logInButton: UIButton!
+    @IBOutlet weak var cpLabel: UILabel!
     @IBOutlet weak var createAccount: UIButton!
+    @IBOutlet weak var cpTF: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        cpTF.isHidden = true
+        cpLabel.isHidden = true
+        createAccount.isHidden = true
         
         addNavBarImage()
 
@@ -25,6 +31,20 @@ class loginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func segControl(_ sender: Any) {
+        switch segControl.selectedSegmentIndex {
+        case 0:
+            cpTF.isHidden = true
+            cpLabel.isHidden = true
+            createAccount.isHidden = true
+        case 1:
+            cpTF.isHidden = false
+            cpLabel.isHidden = false
+            createAccount.isHidden = false
+        default:
+            break
+        }
+    }
     @IBAction func logInPressed(_ sender: Any) {
         Auth.auth().signIn(withEmail: userTF.text!, password: passwordTF.text!) {
             authResult, error in
@@ -40,7 +60,15 @@ class loginViewController: UIViewController {
     }
     
     @IBAction func createAccountPressed(_ sender: Any) {
-        
+        Auth.auth().createUser(withEmail: userTF.text!, password: passwordTF.text!) { [self]
+                    authResult, error in
+                    if let error = error as NSError? {
+                        self.statusLabel.text = "\(error.localizedDescription)"
+                    }
+                    else {
+                        self.statusLabel.text = ""
+                    }
+                }
         
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
