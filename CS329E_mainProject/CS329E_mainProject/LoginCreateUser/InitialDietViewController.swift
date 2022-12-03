9130909e1100f1368b45b924cbebd9b2e5788920
Krystal <eddithfigkyto@gmail.com>
2022-12-03 03:09:02 -0600

    initial diet VC outlets and actions

--- CS329E_mainProject/CS329E_mainProject/LoginCreateUser/InitialDietViewController.swift
+++ CS329E_mainProject/CS329E_mainProject/LoginCreateUser/InitialDietViewController.swift
@@ -8,22 +8,43 @@
 import UIKit
 
 class InitialDietViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var instructionsLabel: UILabel!
+    @IBOutlet weak var selectAllApplyLabel: UILabel!
+    @IBOutlet weak var vegetarianLabel: UILabel!
+    @IBOutlet weak var veganLabel: UILabel!
+    @IBOutlet weak var glutenFreeLabel: UILabel!
+    @IBOutlet weak var nutFreeLabel: UILabel!
+    @IBOutlet weak var halalLabel: UILabel!
+    
+    @IBOutlet weak var vegetarianSwitch: UISwitch!
+    @IBOutlet weak var veganSwitch: UISwitch!
+    @IBOutlet weak var glutenFreeSwitch: UISwitch!
+    @IBOutlet weak var nutFreeSwitch: UISwitch!
+    @IBOutlet weak var halalSwitch: UISwitch!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func vegetarianSelected(_ sender: UISwitch) {
     }
-    */
+    
+    @IBAction func veganSelected(_ sender: UISwitch) {
+    }
+    
+    @IBAction func glutenFreeSelected(_ sender: UISwitch) {
+    }
+    
+    @IBAction func nutFreeSelected(_ sender: UISwitch) {
+    }
+    
+    @IBAction func halalSelected(_ sender: UISwitch) {
+    }
+    
 
 }
