46b0c9093fdf8dfc25afbb9159f59b239a3925b6
Carla Gonzalez <carlagonzalez@utexas.edu>
2022-12-04 00:13:36 -0600

    .

--- CS329E_mainProject/CS329E_mainProject/LoginCreateUser/loginViewController.swift
+++ CS329E_mainProject/CS329E_mainProject/LoginCreateUser/loginViewController.swift
@@ -117,7 +117,7 @@ class loginViewController: UIViewController {
                         self.statusLabel.text = "\(error.localizedDescription)"
                     }
                     else {
-                        self.statusLabel.text = "Account created"
+                        self.statusLabel.text = "Account created, ready to log in."
                     }
                 }
     }
